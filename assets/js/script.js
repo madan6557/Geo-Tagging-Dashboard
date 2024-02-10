dfb93ef5e57c9a986bd24e3a3e4e0d178c076ad3
Muhammad Rizky Aulia Ramadhan <muhammadrizky.mrar@gmail.com>
2024-02-10 11:40:52 +0800

    menambahkan interaksi Bar Navigasi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-//there nothing in here yet
+var nav = document.getElementById("nav-button");
+var navbar = document.getElementById("navbar");
+console.log(navbar);
+
+nav.addEventListener("click", function() {navVisible(navbar);});
+
+function navVisible(element) {
+    if (element.classList.contains("hidden")) {
+        element.classList.remove("hidden");
+    } else {
+        element.classList.add("hidden");
+    }
+}
