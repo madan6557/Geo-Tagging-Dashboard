d73547793dadfbe0de9f493789cec96db6be97ed
Muhammad Rizky Aulia Ramadhan <muhammadrizky.mrar@gmail.com>
2024-02-26 09:54:01 +0800

    last

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
 var nav = document.getElementById("nav-button");
 var navbar = document.getElementById("navbar");
-console.log(navbar);
+var sort = document.getElementById("sortBy");
 
 nav.addEventListener("click", function () { setVisible(navbar); });
+sort.addEventListener("click", function () { sortBy(sort); });
+
+function sortBy(element) {
+    if (element.classList.contains("asc")) {
+        element.classList.remove("asc")
+        element.classList.add("desc")
+    } else {
+        element.classList.remove("desc")
+        element.classList.add("asc")
+    }
+}
 
 function setVisible(element) {
     if (element.classList.contains("hidden")) {
